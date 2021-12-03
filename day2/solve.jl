54ddc5f7d1be88c704372b65138c46da06a00430
Nelson Elhage <nelhage@nelhage.com>
2021-12-02 17:39:33 -0800

    ok

--- day2/solve.jl
+++ day2/solve.jl
@@ -1,29 +1,29 @@
-depth = 0
-horiz = 0
-aim = 0
+function main()
+    depth = 0
+    horiz = 0
+    aim = 0
 
-for line in eachline("input.txt")
-    global depth
-    global horiz
-    global aim
+    for line = eachline("input.txt")
+        m = match(r"(\w+) (\d+)", line)
+        if isnothing(m)
+            error("bad line")
+        end
+        cmd, n = m.captures
+        n = parse(Int, n)
 
-    m = match(r"(\w+) (\d+)", line)
-    if isnothing(m)
-        error("bad line")
+        if cmd == "forward"
+            horiz += n
+            depth += aim * n
+        elseif cmd == "up"
+            aim -= n
+        elseif cmd == "down"
+            aim += n
+        else
+            error("bad cmd: $line")
+        end
     end
-    cmd, n = m.captures
-    n = parse(Int, n)
 
-    if cmd == "forward"
-        horiz += n
-        depth += aim * n
-    elseif cmd == "up"
-        aim -= n
-    elseif cmd == "down"
-        aim += n
-    else
-        error("bad cmd: $line")
-    end
+    println(depth*horiz)
 end
 
-println(depth*horiz)
+main()
